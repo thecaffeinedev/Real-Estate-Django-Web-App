1be7ad0623747f7620d0580a1282e5bae87be36e
unknown <prabhat1015@gmail.com>
2018-12-18 22:21:28 +0530

    Django Files

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from .models import Listing
+
 # Create your views here.
 def index(request):
     return render(request, 'listings/listings.html ')
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html ')
 
 def search(request):
