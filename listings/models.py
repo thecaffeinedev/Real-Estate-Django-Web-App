78dcf90de15957f6f822905b50a780cd75001cc4
unknown <prabhat1015@gmail.com>
2018-12-18 20:37:30 +0530

    Django Files

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Realtors(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
