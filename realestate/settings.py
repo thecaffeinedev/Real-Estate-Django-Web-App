a274407d7c562cf406f0f4e1ed30a4fa257ec3d7
unknown <prabhat1015@gmail.com>
2018-12-18 18:24:31 +0530

    Django Files

--- realestate/settings.py
+++ realestate/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'real_estate' ,
+        'USER': 'postgres',
+        'PASSWORD': 'prateek',
+        'HOST':'localhost',
+        
     }
 }
 
