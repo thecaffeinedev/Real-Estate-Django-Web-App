52113a956817ca99f521f94fec0365930c82b3f4
unknown <prabhat1015@gmail.com>
2018-12-18 21:43:16 +0530

    Django Files

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+  list_display_links = ('id', 'title')
+  list_filter = ('realtor',)
+  list_editable = ('is_published',)
+  search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+  list_per_page = 25
+
+admin.site.register(Listing, ListingAdmin)
