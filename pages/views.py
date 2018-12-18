39b58fa5c2bcebe05c31da1367621a9cc9773074
unknown <prabhat1015@gmail.com>
2018-12-18 23:39:53 +0530

    Django Files

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 
-# Create your views here.
+from listings.models import Listing
+from realtors.models import Realtor
+
 def index(request):
-    return render(request,'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request,'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
