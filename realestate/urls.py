bb26b43d4a42dc9387c0fa7266a323c62ac7fe8d
unknown <prabhat1015@gmail.com>
2018-12-19 00:19:15 +0530

    Django Files

--- realestate/urls.py
+++ realestate/urls.py
@@ -4,7 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('pages.urls')),
-    path( 'listings/', include('listings.urls')),
+    path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
