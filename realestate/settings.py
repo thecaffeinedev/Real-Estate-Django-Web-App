1489bad0930584e304e5c1b7b5a861d760aa494a
Prabhat Kumar Sahu <prabhat1015@gmail.com>
2019-07-23 11:36:29 +0530

    Update settings.py

--- realestate/settings.py
+++ realestate/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'real_estate' ,
         'USER': 'postgres',
-        'PASSWORD': 'prateek',
+        'PASSWORD': '',
         'HOST':'localhost',
         
     }
