bc72e01635da21de7c88df4defda9dd1aabea776
Prabhat Kumar Sahu <prabhat1015@gmail.com>
2021-08-30 00:27:30 +0530

    Update views.py

--- contacts/views.py
+++ contacts/views.py
@@ -30,8 +30,8 @@ def contact(request):
     # send_mail(
     #   'Property Listing Inquiry',
     #   'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-    #   'traversy.brad@gmail.com',
-    #   [realtor_email, 'techguyinfo@gmail.com'],
+    #   'iprabhatdev@gmail.com',
+    #   [realtor_email, 'random@gmail.com'],
     #   fail_silently=False
     # )
 
